5492fbd0e011b063d51c04e20b6ea848a6b4db0d
dhanashekara <51747051+dhanashekara@users.noreply.github.com>
2019-07-01 04:57:52 +0000

    Update CustomerController.java

--- src/main/java/com/bank/customer/controller/CustomerController.java
+++ src/main/java/com/bank/customer/controller/CustomerController.java
@@ -53,7 +53,5 @@ public class CustomerController {
 	public ResponseEntity<JSONArray> getAllCustomers() throws IOException, ParseException {
 		return new ResponseEntity<JSONArray>(customerService.getAllCustomers(), HttpStatus.OK);
 	}
-	/*
-	*/
 
 }
